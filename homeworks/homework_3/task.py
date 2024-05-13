5201955e0220d86ba0b2c89c12fa0fddbcc2980d
alex-s93 <solodkov.alex@gmail.com>
2024-05-13 15:01:13 +0200

    django: add tasks descriptions to homework 3

--- homeworks/homework_3/task.py
+++ homeworks/homework_3/task.py
@@ -9,23 +9,36 @@ from sqlalchemy import (
 )
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+# Задача 1: Создайте экземпляр движка для подключения к SQLite базе данных в памяти.
 engine = create_engine('mysql+pymysql://root:<<password>>@localhost:3306/test_sqlalchemy')
-Base = declarative_base()
 
+# Задача 2: Создайте сессию для взаимодействия с базой данных, используя созданный движок.
 Session = sessionmaker(bind=engine)
-
 session = Session()
 
+Base = declarative_base()
 
+
+# Задача 3: Определите модель продукта Product со следующими типами колонок:
+#     id: числовой идентификатор
+#     name: строка (макс. 100 символов)
+#     price: числовое значение с фиксированной точностью
+#     in_stock: логическое значение
 class Product(Base):
     __tablename__ = 'hw_3_products'
     id = Column(Integer, primary_key=True)
     name = Column(String(100))
     price = Column(Numeric(7, 2))
     in_stock = Column(Boolean)
+
+    #Задача 5: Установите связь между таблицами Product и Category с помощью колонки category_id.
     category_id = Column(Integer, ForeignKey('hw_3_categories.id'))
 
 
+# Задача 4: Определите связанную модель категории Category со следующими типами колонок:
+#     id: числовой идентификатор
+#     name: строка (макс. 100 символов)
+#     description: строка (макс. 255 символов)
 class Category(Base):
     __tablename__ = "hw_3_categories"
     id = Column(Integer, primary_key=True)
